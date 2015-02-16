d2d5147f7086bc9cb1664dde3561ab97f2796d02
Bret Hansen <bret.hansen@ellucian.com>
2015-02-16 08:58:48 -0700

    Initial Commit

---
+++ Ellucian Demo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Ellucian Demo
+//
+//  Created by Bret Hansen on 2/16/15.
+//  Copyright (c) 2015 Ellucian. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Ellucian Demo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Ellucian Demo
+//
+//  Created by Bret Hansen on 2/16/15.
+//  Copyright (c) 2015 Ellucian. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
